653529fb72b498b7e660ca71f2ba0935f9cf2276
Henrique Tavares <ihenrits@gmail.com>
2019-07-13 13:53:37 -0300

    Query & Route params

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ server.get("/json", (req, res) => {
   return res.json({ message: "Hello World" });
 });
 
+// Query Params = ?teste=1
+server.get("/query", (req, res) => {
+  const nome = req.query.nome;
+
+  return res.json({ message: `Hello ${nome}` });
+});
+
+// Route Params = /users/1
+server.get("/routes/:id", (req, res) => {
+  const { id } = req.params;
+
+  return res.json({ message: `Buscando o usuário ${id}` });
+});
+
+// Request Body = { "name": "Henrique Tavares", "email": "ihenrits@gmail.com" }
+
 server.listen(3000);
