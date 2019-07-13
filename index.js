2c9e173f1d9fb317bfad7a7742ca804afa93864d
Henrique Tavares <ihenrits@gmail.com>
2019-07-13 14:46:45 -0300

    CRUD

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/send", (req, res) => {
   return res.send("Hello World");
 });
@@ -25,5 +27,31 @@ server.get("/routes/:id", (req, res) => {
 });
 
 // Request Body = { "name": "Henrique Tavares", "email": "ihenrits@gmail.com" }
+const users = ["Diego", "Henrique"];
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
 
 server.listen(3000);
